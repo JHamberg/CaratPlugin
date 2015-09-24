41fcc6e369311f89ae55e2656f227905974a68c6
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2015-09-24 18:47:58 +0300

    Modified threading, added javadoc

--- src/org/carat20/client/Carat.java
+++ src/org/carat20/client/Carat.java
@@ -1,56 +1,95 @@
 package org.carat20.client;
+
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import android.util.Log;
 
 import org.carat20.client.protocol.CommunicationManager;
 import org.carat20.client.storage.DataStorage;
-import org.apache.thrift.TException;
+import org.carat20.client.thrift.HogBugReport;
 import org.carat20.client.thrift.Reports;
 
-public class Carat extends CordovaPlugin{
+/**
+ * This class acts as the middleware between Phonegap and native functions of
+ * Carat. It contains methods necessary for calling the code from Javascript,
+ * initializing the background process, refreshing/fetching data and providing
+ * the requested data.
+ *
+ * <p>
+ * Class and method snippets are based on the work by original author
+ * <a href="https://github.com/lagerspetz">Eemil Lagerspetz</a>.
+ * </p>
+ *
+ * @author Jonatan Hamberg
+ * @see CommunicationManager
+ * @see DataStorage
+ */
+public class Carat extends CordovaPlugin {
 
     private static DataStorage storage = null;
     private static CommunicationManager communicationManager = null;
+    
+    private int jscore;
+    private String reports;
 
+    /**
+     * Provides an interface for cordova exec which accepts data requests and
+     * fulfills them by either utilizing the server or local storage. Callbacks
+     * are mostly used for passing reports to globally accessible Javascript
+     * objects.
+     *
+     * @param action Determines the function call.
+     * @param args Optional information about the request, e.g. events.
+     * @param callbackContext Used for returning data to callback functions.
+     * @return State boolean, which is true if an action gets executed.
+     * @throws JSONException In case the JSONArray used for args is invalid.
+     */
     @Override
-     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         storage = new DataStorage();
-        Log.v("Carat", "Setting storage, storage is "+storage);
-        new Thread() {
+
+        //Create communicationManager in it's own thread to avoid blocking WebCore
+        cordova.getThreadPool().execute(new Runnable() {
             @Override
             public void run() {
                 communicationManager = new CommunicationManager(storage);
+                while (communicationManager == null) {}
+                communicationManager.refreshAllReports();
+                jscore = getJscore();
+                callbackContext.success(jscore);
             }
-        }.start();
-
-        //Complete freshness-implementation before running on refreshUi
-        while(communicationManager == null){}
-        Log.v("Carat", "Making sure CM is initialized, CM is "+communicationManager);
-        try {
-            communicationManager.refreshAllReports();
-        } catch(TException ex){
-            Log.v("Carat", "Exception when calling RefreshAllReports(): "+ex);
-        }
-
-        if (action.equals("echo")) {
-            int message = this.getJscore();
-            Log.v("Carat", "Getting JScore, message is "+message);
-            callbackContext.success("Jscore is "+message+"!");
-            return true;
-        }
-        return false;
+        });
+        return true;
     }
-     
-    public static int getJscore(){
-        while(storage.getReports()==null){}
-        Log.v("Carat", "Getting reports for JScore, reports are "+storage.getReports());
+
+    /**
+     * Makes a storage call to get main reports from which a JScore is
+     * extracted. We must make sure the background process has finished so that
+     * reports don't get called as null.
+     *
+     * @return JScore, the original value from server multiplied by 100.
+     */
+    public static int getJscore() {
+        //Again, a dirty workaround
+        while (storage.getReports() == null) {}
         Reports reports = storage.getReports();
-        Log.v("Carat", "Reports.getJScore() returned with "+reports.getJScore());
-        return (int)(reports.getJScore()*100);
+        return (int) (reports.getJScore() * 100);
+    }
+
+    /**
+     * Makes a storage call to get reports from which Hog and Bug reports are
+     * extracted. We must make sure the background process has finished so that
+     * reports don't get called as null.
+     *
+     * @return HogBugReport converted to String format.
+     */
+    public static String getHogBugs() {
+
+        //Let's not make this a habit
+        while (storage.getHogBugReports() == null) {}
+        HogBugReport reports = storage.getHogBugReports();
+        return reports.toString();
     }
 }
--- src/org/carat20/client/protocol/CommunicationManager.java
+++ src/org/carat20/client/protocol/CommunicationManager.java
@@ -4,50 +4,110 @@ import android.util.Log;
 import org.carat20.client.storage.DataStorage;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.thrift.TException;
 import org.carat20.client.thrift.CaratService;
 import org.carat20.client.thrift.Feature;
+import org.carat20.client.thrift.HogBugReport;
 import org.carat20.client.thrift.Reports;
 
+/**
+ * Communication middleman between {@link ProtocolClient} and {@link DataStorage}.
+ * It provides the means necessary for fetching data based on client information and 
+ * writing that data in memory for later use. 
+ * 
+ * @author Jonatan Hamberg
+ */
 public class CommunicationManager {
 
     private CaratService.Client instance;
-    private DataStorage dataStorage;
+    private final DataStorage dataStorage;
     
-    public CommunicationManager(DataStorage dataStorage){
+    // Unique identifier, device model and operating system version.
+    private String uuid, model, os;
+
+    /**
+     * Constructor for Communication manager which sets the data storage.
+     * @param dataStorage Used for writing reports in memory. 
+     */
+    public CommunicationManager(DataStorage dataStorage) {
+        if(dataStorage == null){
+            Log.v("Carat", "Failed to construct CommunicationManager, "
+                    + "DataStorage is null");
+            System.exit(1);
+        }
         this.dataStorage = dataStorage;
     }
-    
-    public void refreshAllReports() throws TException {
-        String uuid = "19db5bb46aa305ae";
-        String model = "GT-I9505";
-        String OS = "5.0.1";
-        
-        this.refreshMainReports(uuid, model, OS);
-    }
-    
-    private void refreshMainReports(String uuid, String model, String os) throws TException{
-        instance = ProtocolClient.open();
-        Log.v("Carat", "Opening instance, instance is "+instance);
-        Reports r = instance.getReports(uuid, getFeatures("Model", model, "OS", os));
-        Log.v("Carat", "Getting reports, reports are "+r);
-        dataStorage.writeReports(r);
-        //Close protocol safely
-        //...
-    }
-    
-    	private List<Feature> getFeatures(String key1, String val1, String key2, String val2) {
-		List<Feature> features = new ArrayList<>();
-		if (key1 == null || val1 == null || key2 == null || val2 == null) {
-			System.exit(1);
-			return features;
-		}
-		Feature feature = new Feature();
-                feature.setKey(key1).setValue(val1);
-		features.add(feature);
 
-		feature = new Feature().setKey(key2).setValue(val2);
-		features.add(feature);
-		return features;
-	}
+    /** Responsible for initializing Hogs, Bugs and JScore refresh. */
+    public void refreshAllReports() {
+        
+        //Replace this with real data
+        this.uuid = "19db5bb46aa305ae";
+        this.model = "GT-I9505";
+        this.os = "5.0.1";
+            
+        //Add branching for failed execution
+        this.refreshJscore();
+        this.refreshHogsBugs("Hog");
+        this.refreshHogsBugs("Bug");
+    }
+
+    /** Refreshes JScore by requesting main reports from {@link CaratService} instance.
+     * @return State boolean, which is true if a connection can be opened and reports fetched.
+     */
+    public boolean refreshJscore() {
+        try {
+            instance = ProtocolClient.open();
+            Reports r = instance.getReports(uuid, 
+                    createFeatureList(
+                            "Model", model, 
+                            "OS", os)
+            );
+            dataStorage.writeReports(r);
+            ProtocolClient.close(instance);
+            return true;
+        } catch (Throwable error) {
+            Log.v("Carat", "Error refreshing main reports: " + error);
+        }
+        return false;
+    }
+    
+    /** Refreshes Hog and Bug reports by requesting reports from {@link CaratService} instance.
+     * @param reportType Determines whether the data should be hogs or bugs.
+     * @return State boolean, which is true if a connection can be opened and reports fetched.
+     */
+    public boolean refreshHogsBugs(String reportType){
+        try {
+            instance = ProtocolClient.open();
+            HogBugReport r = instance.getHogOrBugReport(uuid, 
+                    createFeatureList(
+                    "ReportType", reportType, 
+                    "Model", model)
+            );
+            dataStorage.writeReports(r);
+            ProtocolClient.close(instance);
+            return true;
+        } catch (Throwable error) {
+            Log.v("Carat", "Error refreshing reports: " + error);
+        }
+        return false;
+    }
+    
+    //Bundles two value-key pairs together in a Feature List
+    private List<Feature> createFeatureList(String key1, String val1, String key2, String val2) {
+        
+        //Diamond inference is not supported
+        List<Feature> features = new ArrayList<Feature>();
+        if (key1 == null || val1 == null || key2 == null || val2 == null) {
+            System.exit(1);
+            return features;
+        }
+        
+        //Feature constructor doesn't return a new object if directly assigned.
+        Feature feature = new Feature().setKey(key1).setValue(val1);
+        features.add(feature);
+        
+        feature = new Feature().setKey(key2).setValue(val2);
+        features.add(feature);
+        return features;
+    }
 }
